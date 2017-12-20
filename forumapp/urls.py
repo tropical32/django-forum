ab7030f3f4141d697b2776eacb6067baa2f55d94
dydokamil <dydokamil@gmail.com>
2017-12-20 18:24:58 +0100

    Added style for forums' rows * Changed name from forum to forums * Navbar displays the logged in user * Added the list view for threads

--- forumapp/urls.py
+++ forumapp/urls.py
@@ -9,8 +9,19 @@ urlpatterns = [
         name='index'
     ),
     path(
-        'forum',
-        views.forum,
+        'forums',
+        views.forums,
+        name='forums'
+    ),
+    path(
+        'forum/<int:pk>',
+        views.ThreadListView.as_view(),
         name='forum'
     ),
+    path(
+        'new-thread/<int:pk>',
+        views.CreateThread.as_view(),
+        name='new-thread'
+    )
+
 ]
--- forumapp/views.py
+++ forumapp/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 from .models import Thread, Forum, ForumSection
 
@@ -12,14 +12,22 @@ def index(request):
     )
 
 
-def forum(request):
+def forums(request):
     sections = ForumSection.objects.all()
     forums = Forum.objects.all()
     return render(
         request,
-        'forumapp/forum.html',
+        'forumapp/forums.html',
         context={
             'sections': sections,
             # 'forums': forums,
         }
     )
+
+
+class ThreadListView(ListView):
+    model = Thread
+
+
+class CreateThread(CreateView):
+    model = Thread
