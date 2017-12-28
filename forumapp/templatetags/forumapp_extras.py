5563e3c904a5b2d0b92afa9ee117974807c20b2a
kamil <dydokamil@gmail.com>
2017-12-28 11:38:33 +0100

    Stylization for pagination navbar

--- forumapp/templatetags/forumapp_extras.py
+++ forumapp/templatetags/forumapp_extras.py
@@ -7,3 +7,13 @@ register = template.Library()
 def can_remove_response(response, user):
     return user == response.responder or \
            user.has_perm('forumapp.can_remove_any_response')
+
+
+@register.filter(name='times0')
+def times0(count):
+    return ''.join([str(num) for num in range(count)])
+
+
+@register.filter(name='times')
+def times(count):
+    return ''.join([str(num) for num in range(1, count + 1)])
